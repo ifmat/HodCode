7b4d079d8f8bb6397fc0cc6d248909a9e8c4aa18
Matin <youremail@example.com09363880280a@gmail.com>
2025-08-08 15:55:50 +0330

    اولین کامیت

---
+++ test1.php
@@ -0,0 +1,19 @@
+<?php
+for ($i=1; $i <= 16; $i++ ) {
+    if ($i % 2 == 0) {
+        echo "$i squrad:".$i *$i;
+//        echo "<dive style='bottom: 1vh ;border: 2px solid black;'></div>";
+        echo "<br>";
+
+    }
+};
+echo "<hr>";
+$i=1;
+while($i<=16){
+    if ($i % 2 == 0) {
+        echo '$i squrad:'.$i *$i;
+        echo "<br>";
+
+    }
+    $i++;
+}
